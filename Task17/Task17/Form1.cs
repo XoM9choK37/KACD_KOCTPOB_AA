8d2a17ab85846085d8b3d595e4e43d499a631225
XoM9choK37 <sanya.sasha.kostrov@mail.ru>
2024-12-07 14:25:29 +0300

    Add files via upload

---
+++ Task17/Task17/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Task17
+{
+    public partial class Form1 : Form
+    {
+        const int TIME1 = 3_000;
+        const int TIME2 = 5_000;
+        const int TIME3 = 7_000;
+        const int TIME4 = 10_000;
+
+        int state = 0;
+        GraphPane pane;
+        PointPairList listAL = new PointPairList();
+        PointPairList listLL = new PointPairList();
+        double[][] times;
+        double[] averages;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            zedGraphControl1.Hide();
+            label1.Hide();
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                switch (comboBox1.SelectedIndex)
+                {
+                    case 0:
+                        switch (comboBox2.SelectedIndex)
+                        {
+                            case 0:
+                                times = Lists.GetTimes
+                                (Lists.GetSpeedTest,
+                                TIME1);
+                                break;
+                            case 1:
+                                times = Lists.GetTimes
+                                (Lists.GetSpeedTest,
+                                TIME2);
+                                break;
+                            case 2:
+                                times = Lists.GetTimes
+                                (Lists.GetSpeedTest,
+                                TIME3);
+                                break;
+                            case 3:
+                                times = Lists.GetTimes
+                                (Lists.GetSpeedTest,
+                                TIME4);
+                                break;
+                        }
+                        break;
+                    case 1:
+                        switch (comboBox2.SelectedIndex)
+                        {
+                            case 0:
+                                times = Lists.GetTimes
+                                (Lists.SetSpeedTest,
+                                TIME1);
+                                break;
+                            case 1:
+                                times = Lists.GetTimes
+                                (Lists.SetSpeedTest,
+                                TIME2);
+                                break;
+                            case 2:
+                                times = Lists.GetTimes
+                                (Lists.SetSpeedTest,
+                                TIME3);
+                                break;
+                            case 3:
+                                times = Lists.GetTimes
+                                (Lists.SetSpeedTest,
+                                TIME4);
+                                break;
+                        }
+                        break;
+                    case 2:
+                        switch (comboBox2.SelectedIndex)
+                        {
+                            case 0:
+                                times = Lists.GetTimes
+                                (Lists.AddSpeedTest,
+                                TIME1);
+                                break;
+                            case 1:
+                                times = Lists.GetTimes
+                                (Lists.AddSpeedTest,
+                                TIME2);
+                                break;
+                            case 2:
+                                times = Lists.GetTimes
+                                (Lists.AddSpeedTest,
+                                TIME3);
+                                break;
+                            case 3:
+                                times = Lists.GetTimes
+                                (Lists.AddSpeedTest,
+                                TIME4);
+                                break;
+                        }
+                        break;
+                    case 3:
+                        switch (comboBox2.SelectedIndex)
+                        {
+                            case 0:
+                                times = Lists.GetTimes
+                                (Lists.AddAtSpeedTest,
+                                TIME1);
+                                break;
+                            case 1:
+                                times = Lists.GetTimes
+                                (Lists.AddAtSpeedTest,
+                                TIME2);
+                                break;
+                            case 2:
+                                times = Lists.GetTimes
+                                (Lists.AddAtSpeedTest,
+                                TIME3);
+                                break;
+                            case 3:
+                                times = Lists.GetTimes
+                                (Lists.AddAtSpeedTest,
+                                TIME4);
+                                break;
+                        }
+                        break;
+                    case 4:
+                        switch (comboBox2.SelectedIndex)
+                        {
+                            case 0:
+                                times = Lists.GetTimes
+                                (Lists.RemoveSpeedTest,
+                                TIME1);
+                                break;
+                            case 1:
+                                times = Lists.GetTimes
+                                (Lists.RemoveSpeedTest,
+                                TIME2);
+                                break;
+                            case 2:
+                                times = Lists.GetTimes
+                                (Lists.RemoveSpeedTest,
+                                TIME3);
+                                break;
+                            case 3:
+                                times = Lists.GetTimes
+                                (Lists.RemoveSpeedTest,
+                                TIME4);
+                                break;
+                        }
+                        break;
+                }
+                if (0 <= comboBox1.SelectedIndex &&
+                    comboBox1.SelectedIndex <= 4 &&
+                    0 <= comboBox2.SelectedIndex &&
+                    comboBox2.SelectedIndex <= 3)
+                {
+                    zedGraphControl1.AxisChange();
+                    zedGraphControl1.Invalidate();
+
+                    zedGraphControl1.Show();
+                    pane = zedGraphControl1.GraphPane;
+                    pane.Legend.FontSpec.Size = 16;
+                    pane.Title.Text = "Анализ эффективности массива и списка";
+                    pane.XAxis.Title.Text = "Номер теста";
+                    pane.YAxis.Title.Text = "Время выполнения операций (мс)";
+
+                    pane.CurveList.Clear();
+                    listAL.Clear();
+                    listLL.Clear();
+
+                    averages = Lists.GetMatrixAverage(times);
+                    for (int i = 0; i < 20; i++)
+                        listAL.Add(i + 1, times[i][0]);
+                    LineItem curveAL = pane.AddCurve("MyArrayList\t", listAL,
+                        Color.Red, SymbolType.None);
+                    for (int i = 0; i < 20; i++)
+                        listLL.Add(i + 1, times[i][1]);
+                    LineItem curveLL = pane.AddCurve("MyLinkedList\t", listLL,
+                        Color.Blue, SymbolType.None);
+                    label1.Text = "Среднее время выполнения операций\n" +
+                        "MyArrayList: " + averages[0] + " мс\n" +
+                        "MyLinkedList: " + averages[1] + " мс";
+                    label1.Show();
+                    pane.XAxis.Scale.Min = 1;
+                    pane.XAxis.Scale.Max = 20;
+
+                    zedGraphControl1.AxisChange();
+                    zedGraphControl1.Invalidate();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Task17/Task17/Lists.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task4;
+using Task16;
+using System.Runtime.InteropServices;
+using System.Collections;
+using System.Drawing;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+
+namespace Task17
+{
+    static class Lists
+    {
+        const int MODULE = 1_000;
+        public static void ArrayListGet(MyArrayList<int> arrayList, int amount)
+        {
+            Random random = new Random();
+            for (int i = 0; i < amount; i++)
+                arrayList.Get(random.Next(arrayList.Size()));
+        }
+        public static void LinkedListGet(MyLinkedList<int> linkedList, int amount)
+        {
+            Random random = new Random();
+            for (int i = 0; i < amount; i++)
+                linkedList.Get(random.Next(linkedList.Size()));
+        }
+        public static void ArrayListSet(MyArrayList<int> arrayList, int amount)
+        {
+            Random random = new Random();
+            for (int i = 0; i < amount; i++)
+                arrayList.Set(random.Next(arrayList.Size()), random.Next(MODULE));
+        }
+        public static void LinkedListSet(MyLinkedList<int> linkedList, int amount)
+        {
+            Random random = new Random();
+            for (int i = 0; i < amount; i++)
+                linkedList.Set(random.Next(linkedList.Size()), random.Next(MODULE));
+        }
+        public static void ArrayListAdd(MyArrayList<int> arrayList, int amount)
+        {
+            Random random = new Random();
+            for (int i = 0; i < amount; i++)
+                arrayList.Add(random.Next(MODULE));
+        }
+        public static void LinkedListAdd(MyLinkedList<int> linkedList, int amount)
+        {
+            Random random = new Random();
+            for (int i = 0; i < amount; i++)
+                linkedList.Add(random.Next(MODULE));
+        }
+        public static void ArrayListAddAt(MyArrayList<int> arrayList, int amount)
+        {
+            Random random = new Random();
+            for (int i = 0; i < amount; i++)
+                arrayList.Add(random.Next(arrayList.Size()), random.Next(MODULE));
+        }
+        public static void LinkedListAddAt(MyLinkedList<int> linkedList, int amount)
+        {
+            Random random = new Random();
+            for (int i = 0; i < amount; i++)
+                linkedList.Add(random.Next(linkedList.Size()), random.Next(MODULE));
+        }
+        public static void ArrayListRemove(MyArrayList<int> arrayList)
+        {
+            Random random = new Random();
+            for (int i = 0; i < arrayList.Size(); i++)
+                arrayList.Remove(index: random.Next(arrayList.Size()));
+        }
+        public static void LinkedListRemove(MyLinkedList<int> linkedList)
+        {
+            Random random = new Random();
+            for (int i = 0; i < linkedList.Size(); i++)
+                linkedList.RemoveAt(random.Next(linkedList.Size()));
+        }
+        public static double[] AddSpeedTest(int amount)
+        {
+            double[] time = new double[2];
+            Stopwatch stopwatch = new Stopwatch();
+            MyArrayList<int> arrayList = new MyArrayList<int>();
+            MyLinkedList<int> linkedList = new MyLinkedList<int>();
+            int element;
+            Random random = new Random();
+            for (int i = 0; i < amount; i++)
+            {
+                element = random.Next();
+                arrayList.Add(element);
+                linkedList.Add(element);
+            }
+            stopwatch.Start();
+            ArrayListAdd(arrayList, amount);
+            stopwatch.Stop();
+            time[0] = stopwatch.ElapsedMilliseconds;
+            stopwatch.Reset();
+            stopwatch.Start();
+            LinkedListAdd(linkedList, amount);
+            stopwatch.Stop();
+            time[1] = stopwatch.ElapsedMilliseconds;
+            return time;
+        }
+        public static double[] GetSpeedTest(int amount)
+        {
+            double[] time = new double[2];
+            Stopwatch stopwatch = new Stopwatch();
+            MyArrayList<int> arrayList = new MyArrayList<int>();
+            MyLinkedList<int> linkedList = new MyLinkedList<int>();
+            int element;
+            Random random = new Random();
+            for (int i = 0; i < amount; i++)
+            {
+                element = random.Next();
+                arrayList.Add(element);
+                linkedList.Add(element);
+            }
+            stopwatch.Start();
+            ArrayListGet(arrayList, amount);
+            stopwatch.Stop();
+            time[0] = stopwatch.ElapsedMilliseconds;
+            stopwatch.Reset();
+            stopwatch.Start();
+            LinkedListGet(linkedList, amount);
+            stopwatch.Stop();
+            time[1] = stopwatch.ElapsedMilliseconds;
+            return time;
+        }
+        public static double[] SetSpeedTest(int amount)
+        {
+            double[] time = new double[2];
+            Stopwatch stopwatch = new Stopwatch();
+            MyArrayList<int> arrayList = new MyArrayList<int>();
+            MyLinkedList<int> linkedList = new MyLinkedList<int>();
+            int element;
+            Random random = new Random();
+            for (int i = 0; i < amount; i++)
+            {
+                element = random.Next();
+                arrayList.Add(element);
+                linkedList.Add(element);
+            }
+            stopwatch.Start();
+            ArrayListSet(arrayList, amount);
+            stopwatch.Stop();
+            time[0] = stopwatch.ElapsedMilliseconds;
+            stopwatch.Reset();
+            stopwatch.Start();
+            LinkedListSet(linkedList, amount);
+            stopwatch.Stop();
+            time[1] = stopwatch.ElapsedMilliseconds;
+            return time;
+        }
+        public static double[] AddAtSpeedTest(int amount)
+        {
+            double[] time = new double[2];
+            Stopwatch stopwatch = new Stopwatch();
+            MyArrayList<int> arrayList = new MyArrayList<int>();
+            MyLinkedList<int> linkedList = new MyLinkedList<int>();
+            int element;
+            Random random = new Random();
+            for (int i = 0; i < amount; i++)
+            {
+                element = random.Next();
+                arrayList.Add(element);
+                linkedList.Add(element);
+            }
+            stopwatch.Start();
+            ArrayListAddAt(arrayList, amount);
+            stopwatch.Stop();
+            time[0] = stopwatch.ElapsedMilliseconds;
+            stopwatch.Reset();
+            stopwatch.Start();
+            LinkedListAddAt(linkedList, amount);
+            stopwatch.Stop();
+            time[1] = stopwatch.ElapsedMilliseconds;
+            return time;
+        }
+        public static double[] RemoveSpeedTest(int amount)
+        {
+            double[] time = new double[2];
+            Stopwatch stopwatch = new Stopwatch();
+            MyArrayList<int> arrayList = new MyArrayList<int>();
+            MyLinkedList<int> linkedList = new MyLinkedList<int>();
+            int element;
+            Random random = new Random();
+            for (int i = 0; i < amount; i++)
+            {
+                element = random.Next();
+                arrayList.Add(element);
+                linkedList.Add(element);
+            }
+            stopwatch.Start();
+            ArrayListRemove(arrayList);
+            stopwatch.Stop();
+            time[0] = stopwatch.ElapsedMilliseconds;
+            stopwatch.Reset();
+            stopwatch.Start();
+            LinkedListRemove(linkedList);
+            stopwatch.Stop();
+            time[1] = stopwatch.ElapsedMilliseconds;
+            return time;
+        }
+        public static double[] GetMatrixAverage(double[][] matrix)
+        {
+            double[] array = new double[matrix.Length];
+            double sum;
+            for (int j = 0; j < matrix[0].Length; j++)
+            {
+                sum = 0;
+                for (int i = 0; i < matrix.Length; i++)
+                    sum += matrix[i][j];
+                array[j] = sum / matrix.Length;
+            }
+            return array;
+        }
+        public static double[][] GetTimes(Func<int, double[]> func, int amount)
+        {
+            double[][] times = new double[20][];
+            for (int i = 0; i < 20; i++)
+                times[i] = new double[2];
+            for (int i = 0; i < 20; i++)
+            {
+                times[i][0] = func(amount)[0];
+                times[i][1] = func(amount)[1];
+            }
+            return times;
+        }
+    }
+}
